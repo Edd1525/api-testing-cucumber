b483648d92559e105a7656ebc4ac255d748fe556
ebustamantea <ebustamantea@psl.com.co>
2019-10-24 15:33:59 -0500

    close

--- features/steps_definitions/assetCollections.steps.js
+++ features/steps_definitions/assetCollections.steps.js
@@ -1,28 +1,101 @@
-const { Given, When, Then } = require('cucumber');
-const { URLSearchParams } = require('url');
-const request = require('request');
+const { Given, When, Then } = require('cucumber')
+const { URLSearchParams } = require('url')
+const uuidv1 = require('uuid/v1');
+const faker = require('faker')
+const statusCode = require('http-status-codes')
+const request = require('request')
+const { expect } = require('chai')
+let deviceId
 
 Given('a valid admin user', async () => {
+    return 1
+});
 
-    //  const body = { a: 1 };
+Given('a valid integration', function () {
+    const url = 'https://dev-ddp.vmaxlive.net/v2/integrations/testfleet20/VMAXLIVE'
 
-    // const body = {
-    //     "client_id": "7705702d-e6c1-95b7-6851-8b2a300bf057",
-    //     "client_secret": "322eaf21-4cab-b27f-f13a-67690b565a8b",
-    //     "grant_type":"client_credentials"
-    // }
+    const postData = {
+        params: [
+            {
+                name: 'fleet',
+                value: 'testfleet20'
+            },
+            {
+                name: 'application',
+                value: 'VMAXLIVE'
+            }
+        ]
+    }
 
-    // const responseRequest = request.get({
-    //     url: 'https://dev-id.safefleetcloud.com/connect/token',
-    //     body: {
-    //         "client_id": "7705702d-e6c1-95b7-6851-8b2a300bf057",
-    //         "client_secret": "322eaf21-4cab-b27f-f13a-67690b565a8b",
-    //         "grant_type": "client_credentials"
-    //     }
-    // });
+    const options = {
+        method: 'get',
+        headers: {
+            DDPAuthorization: "eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE3NjAxMTg4MTMsImZsZWV0IjoidGVzdGZsZWV0MjAiLCJ1c2VySWQiOiJjcm9tZXJvIiwiYXBwbGljYXRpb24iOiJWTUFYTElWRSIsInNlcnZlciI6InNlcnZlcjEifQ.AFRoWJ36gIpLTVdwJX2ZsalL7oBUbP_H_Ozgm0wH6oZdEZFs7W_-uLXyufIUldcOpHCaoU4KHj8Oqbsu_t8Dsg"
+        },
+        url,
+        body: postData,
+        json: true
+    };
 
-    // var request = require('request')
+    request(options, function (err, response, body) {
+        if (err) {
+            console.error('error posting json: ', err)
+            throw err
+        }
 
+        expect(response.statusCode).to.equal(statusCode.OK);
+        expect(body).to.have.property('data');
+        expect(body.errorCode).to.equal(null);
+    })
+});
+
+Given('a valid vehicle', function () {
+
+    return 1
+
+});
+
+Given('a valid device', function () {
+    const number = faker.random.number(10000);
+
+    const url = 'https://dev-ddp.vmaxlive.net:443/devices'
+
+    const postData = {
+        "deviceId": `Device${number}`,
+        "remoteId": `Device${number}`,
+        "source": "SEON"
+    }
+
+    vehicleId = postData.deviceId
+
+    const options = {
+        headers: {
+            DDPAuthorization: "eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE3NjAxMTg4MTMsImZsZWV0IjoidGVzdGZsZWV0MjAiLCJ1c2VySWQiOiJjcm9tZXJvIiwiYXBwbGljYXRpb24iOiJWTUFYTElWRSIsInNlcnZlciI6InNlcnZlcjEifQ.AFRoWJ36gIpLTVdwJX2ZsalL7oBUbP_H_Ozgm0wH6oZdEZFs7W_-uLXyufIUldcOpHCaoU4KHj8Oqbsu_t8Dsg",
+        },
+        method: 'post',
+        body: postData,
+        json: true,
+        url: url
+    };
+
+    const responseBody = {
+        "errorCode": "null", "data": "CREATE_DEVICE_SUCCESS"
+    }
+
+    request(options, function (err, response, body) {
+        if (err) {
+            console.error('error posting json: ', err)
+            throw err
+        }
+
+        const responseBody  = { errorCode: null, data: 'CREATE_DEVICE_SUCCESS' }
+
+        expect(response.statusCode).to.equal(statusCode.OK);
+        expect(JSON.stringify((body))).to.equal(JSON.stringify(responseBody));
+    })
+});
+
+When('the device is associated to the vehicle', function () {
     const url = 'https://dev-ddp.vmaxlive.net:443/v2/accounts/testfleet20/SEON'
 
     const postData = {
@@ -31,65 +104,63 @@ Given('a valid admin user', async () => {
     const options = {
         method: 'get',
         headers: {
-            Authorization: "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IkVBMjkwQjVBMjM0NzBFOTcyNDRFNkIwMDkzQTVBMDU1MjdBODhCNzMiLCJ0eXAiOiJKV1QiLCJ4NXQiOiI2aWtMV2lOSERwY2tUbXNBazZXZ1ZTZW9pM00ifQ.eyJuYmYiOjE1NzE4NjA4NDUsImV4cCI6MTU3MTg2NDQ0NSwiaXNzIjoiaHR0cHM6Ly9kZXYtaWQuc2FmZWZsZWV0Y2xvdWQuY29tIiwiYXVkIjpbImh0dHBzOi8vZGV2LWlkLnNhZmVmbGVldGNsb3VkLmNvbS9yZXNvdXJjZXMiLCJkZHAtZW5kcG9pbnQtbWFuYWdlciJdLCJjbGllbnRfaWQiOiI3NzA1NzAyZC1lNmMxLTk1YjctNjg1MS04YjJhMzAwYmYwNTciLCJzY29wZSI6WyJkZHAtZW5kcG9pbnQtbWFuYWdlciJdfQ.E08XYKF9tvi4K-jqebUZVsufxryw2IYu5mtwIkp6N450zsVb-j0tZYCFBsLdeAUAZrbkyzqFCTSjGVKvo6sgxqYTQNHQE3NT5XSl5I9VF_XoDpBW5R0Q1QZk4TOVJjA8cP830ULZRd5Ng18t5hu5846Dd7UWzViKG7j9N5iue2MkoxTUChBdkatXyDbYJFWKKpdlBMUW6pWSZeNZ62fpNbWxbipZcRkEhnXpZqv9XBVgrvTjtE09iaa1vOQBTTU0vdo3QJ-k0goWgIutbd1a2evJeEXoLsUU-ldTr2eNp3Lc_etO3eo17WQjLsshcCYVboNk6jA2kcOX7h7sIrt2Rw",
+            Authorization: "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IkVBMjkwQjVBMjM0NzBFOTcyNDRFNkIwMDkzQTVBMDU1MjdBODhCNzMiLCJ0eXAiOiJKV1QiLCJ4NXQiOiI2aWtMV2lOSERwY2tUbXNBazZXZ1ZTZW9pM00ifQ.eyJuYmYiOjE1NzE5NDYyMzYsImV4cCI6MTU3MTk0OTgzNiwiaXNzIjoiaHR0cHM6Ly9kZXYtaWQuc2FmZWZsZWV0Y2xvdWQuY29tIiwiYXVkIjpbImh0dHBzOi8vZGV2LWlkLnNhZmVmbGVldGNsb3VkLmNvbS9yZXNvdXJjZXMiLCJkZHAtZW5kcG9pbnQtbWFuYWdlciJdLCJjbGllbnRfaWQiOiI3NzA1NzAyZC1lNmMxLTk1YjctNjg1MS04YjJhMzAwYmYwNTciLCJzY29wZSI6WyJkZHAtZW5kcG9pbnQtbWFuYWdlciJdfQ.mkh2lqaw_otM8ETXDEJc1QldN1lgOtd0hiGJwOzUXRO-JMtfWZmv5_sZQkLIpJgl_oLIhr0CzOwPQzrASYuZBMBhnEkzPOL_Edps6_csDmEJzVVt_mMZQnxJOplDTvyGYCi98FVqLaLQ6Vd8MhGgdGayms9BDoseIWzfu5DBEEAhrjNlBeYGcJpG8NZA_oV5yzchu1wkAwYrwLxJILBxa-RDVHOfmy1OxZYu-5hm9HtayDMl3cc4JIVSrPQHYYJULjPSgzmvwdiwfPU7kjPsvcynD06_aE1lEw8DGW8w8FMXyCEjKF5g4bHgThtRgP0vpxiyGmMewmLFEw6-qyaQMQ",
         },
-        // port: 443,
         url,
         body: postData,
         json: true
     };
 
-    const requestT = request(options, function (err, res, body) {
+    request(options, function (err, response, body) {
         if (err) {
             console.error('error posting json: ', err)
             throw err
         }
-        var headers = res.headers
-        var statusCode = res.statusCode
-        console.log('headers: ', headers)
-        console.log('statusCode: ', statusCode)
-        console.log('body: ', body)
+
+        const responseBody = { "errorCode": null, "data": [{ "id": 2847, "name": "seon_account", "active": true }] }
+
+        expect(response.statusCode).to.equal(statusCode.OK)
+        expect(JSON.stringify((body))).to.equal(JSON.stringify(responseBody));
+
     })
-
-    console.log('! + > + > + > + > + > + > !  : request', requestT)
-
-    // const requestT = request(options, function (err, res, body) {
-    //     if (err) {
-    //         console.error('error posting json: ', err)
-    //         throw err
-    //     }
-    //     var headers = res.headers
-    //     var statusCode = res.statusCode
-    //     console.log('headers: ', headers)
-    //     console.log('statusCode: ', statusCode)
-    //     console.log('body: ', body)
-    // })
-
-
-
-});
-
-Given('a valid integration', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 1;
-});
-
-Given('a valid vehicle', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 1;
-});
-
-Given('a valid device', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 1;
-});
-
-When('the device is associated to the vehicle', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 1;
 });
 
 Then('the asset collection should be created.', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 1;
+    const url = 'https://dev-ddp.vmaxlive.net:443/v2/assetcollections'
+    const name = faker.name.findName();
+    const notes = faker.lorem.lines(1)
+
+    const postData = {
+        device: {
+            accountId: "2847",
+            id: vehicleId
+        },
+        fleetId: "testfleet20",
+        name,
+        notes,
+        uvId: uuidv1()
+    }
+
+    const options = {
+        headers: {
+            Authorization: "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IkVBMjkwQjVBMjM0NzBFOTcyNDRFNkIwMDkzQTVBMDU1MjdBODhCNzMiLCJ0eXAiOiJKV1QiLCJ4NXQiOiI2aWtMV2lOSERwY2tUbXNBazZXZ1ZTZW9pM00ifQ.eyJuYmYiOjE1NzE5NDYyMzYsImV4cCI6MTU3MTk0OTgzNiwiaXNzIjoiaHR0cHM6Ly9kZXYtaWQuc2FmZWZsZWV0Y2xvdWQuY29tIiwiYXVkIjpbImh0dHBzOi8vZGV2LWlkLnNhZmVmbGVldGNsb3VkLmNvbS9yZXNvdXJjZXMiLCJkZHAtZW5kcG9pbnQtbWFuYWdlciJdLCJjbGllbnRfaWQiOiI3NzA1NzAyZC1lNmMxLTk1YjctNjg1MS04YjJhMzAwYmYwNTciLCJzY29wZSI6WyJkZHAtZW5kcG9pbnQtbWFuYWdlciJdfQ.mkh2lqaw_otM8ETXDEJc1QldN1lgOtd0hiGJwOzUXRO-JMtfWZmv5_sZQkLIpJgl_oLIhr0CzOwPQzrASYuZBMBhnEkzPOL_Edps6_csDmEJzVVt_mMZQnxJOplDTvyGYCi98FVqLaLQ6Vd8MhGgdGayms9BDoseIWzfu5DBEEAhrjNlBeYGcJpG8NZA_oV5yzchu1wkAwYrwLxJILBxa-RDVHOfmy1OxZYu-5hm9HtayDMl3cc4JIVSrPQHYYJULjPSgzmvwdiwfPU7kjPsvcynD06_aE1lEw8DGW8w8FMXyCEjKF5g4bHgThtRgP0vpxiyGmMewmLFEw6-qyaQMQ"
+        },
+        method: 'post',
+        body: postData,
+        json: true,
+        url: url
+    }
+
+    request(options, function (err, response, body) {
+        if (err) {
+            console.error('error posting json: ', err)
+            throw err
+        }
+
+        const responseBody = { errorCode: null, data: 'ASSET_COLLECTION_CREATED' }
+
+        expect(response.statusCode).to.equal(statusCode.CREATED)
+        expect(JSON.stringify((body))).to.equal(JSON.stringify(responseBody));
+
+    })
 });
--- features/steps_definitions/getAccounts.js
+++ features/steps_definitions/getAccounts.js
@@ -1,95 +1,95 @@
-const { Given, When, Then } = require('cucumber');
-const { URLSearchParams } = require('url');
-const request = require('request');
+// const { Given, When, Then } = require('cucumber');
+// const { URLSearchParams } = require('url');
+// const request = require('request');
 
-Given('a valid admin user', async () => {
+// Given('a valid admin user', async () => {
 
-    //  const body = { a: 1 };
+//     //  const body = { a: 1 };
 
-    // const body = {
-    //     "client_id": "7705702d-e6c1-95b7-6851-8b2a300bf057",
-    //     "client_secret": "322eaf21-4cab-b27f-f13a-67690b565a8b",
-    //     "grant_type":"client_credentials"
-    // }
+//     // const body = {
+//     //     "client_id": "7705702d-e6c1-95b7-6851-8b2a300bf057",
+//     //     "client_secret": "322eaf21-4cab-b27f-f13a-67690b565a8b",
+//     //     "grant_type":"client_credentials"
+//     // }
 
-    // const responseRequest = request.get({
-    //     url: 'https://dev-id.safefleetcloud.com/connect/token',
-    //     body: {
-    //         "client_id": "7705702d-e6c1-95b7-6851-8b2a300bf057",
-    //         "client_secret": "322eaf21-4cab-b27f-f13a-67690b565a8b",
-    //         "grant_type": "client_credentials"
-    //     }
-    // });
+//     // const responseRequest = request.get({
+//     //     url: 'https://dev-id.safefleetcloud.com/connect/token',
+//     //     body: {
+//     //         "client_id": "7705702d-e6c1-95b7-6851-8b2a300bf057",
+//     //         "client_secret": "322eaf21-4cab-b27f-f13a-67690b565a8b",
+//     //         "grant_type": "client_credentials"
+//     //     }
+//     // });
 
-    // var request = require('request')
+//     // var request = require('request')
 
-    const url = 'https://dev-ddp.vmaxlive.net:443/v2/accounts/testfleet20/SEON'
+//     const url = 'https://dev-ddp.vmaxlive.net:443/v2/accounts/testfleet20/SEON'
 
-    const postData = {
-    }
+//     const postData = {
+//     }
 
-    const options = {
-        method: 'get',
-        headers: {
-            Authorization: "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IkVBMjkwQjVBMjM0NzBFOTcyNDRFNkIwMDkzQTVBMDU1MjdBODhCNzMiLCJ0eXAiOiJKV1QiLCJ4NXQiOiI2aWtMV2lOSERwY2tUbXNBazZXZ1ZTZW9pM00ifQ.eyJuYmYiOjE1NzE4NjA4NDUsImV4cCI6MTU3MTg2NDQ0NSwiaXNzIjoiaHR0cHM6Ly9kZXYtaWQuc2FmZWZsZWV0Y2xvdWQuY29tIiwiYXVkIjpbImh0dHBzOi8vZGV2LWlkLnNhZmVmbGVldGNsb3VkLmNvbS9yZXNvdXJjZXMiLCJkZHAtZW5kcG9pbnQtbWFuYWdlciJdLCJjbGllbnRfaWQiOiI3NzA1NzAyZC1lNmMxLTk1YjctNjg1MS04YjJhMzAwYmYwNTciLCJzY29wZSI6WyJkZHAtZW5kcG9pbnQtbWFuYWdlciJdfQ.E08XYKF9tvi4K-jqebUZVsufxryw2IYu5mtwIkp6N450zsVb-j0tZYCFBsLdeAUAZrbkyzqFCTSjGVKvo6sgxqYTQNHQE3NT5XSl5I9VF_XoDpBW5R0Q1QZk4TOVJjA8cP830ULZRd5Ng18t5hu5846Dd7UWzViKG7j9N5iue2MkoxTUChBdkatXyDbYJFWKKpdlBMUW6pWSZeNZ62fpNbWxbipZcRkEhnXpZqv9XBVgrvTjtE09iaa1vOQBTTU0vdo3QJ-k0goWgIutbd1a2evJeEXoLsUU-ldTr2eNp3Lc_etO3eo17WQjLsshcCYVboNk6jA2kcOX7h7sIrt2Rw",
-        },
-        // port: 443,
-        url,
-        body: postData,
-        json: true
-    };
+//     const options = {
+//         method: 'get',
+//         headers: {
+//             Authorization: "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IkVBMjkwQjVBMjM0NzBFOTcyNDRFNkIwMDkzQTVBMDU1MjdBODhCNzMiLCJ0eXAiOiJKV1QiLCJ4NXQiOiI2aWtMV2lOSERwY2tUbXNBazZXZ1ZTZW9pM00ifQ.eyJuYmYiOjE1NzE4NjA4NDUsImV4cCI6MTU3MTg2NDQ0NSwiaXNzIjoiaHR0cHM6Ly9kZXYtaWQuc2FmZWZsZWV0Y2xvdWQuY29tIiwiYXVkIjpbImh0dHBzOi8vZGV2LWlkLnNhZmVmbGVldGNsb3VkLmNvbS9yZXNvdXJjZXMiLCJkZHAtZW5kcG9pbnQtbWFuYWdlciJdLCJjbGllbnRfaWQiOiI3NzA1NzAyZC1lNmMxLTk1YjctNjg1MS04YjJhMzAwYmYwNTciLCJzY29wZSI6WyJkZHAtZW5kcG9pbnQtbWFuYWdlciJdfQ.E08XYKF9tvi4K-jqebUZVsufxryw2IYu5mtwIkp6N450zsVb-j0tZYCFBsLdeAUAZrbkyzqFCTSjGVKvo6sgxqYTQNHQE3NT5XSl5I9VF_XoDpBW5R0Q1QZk4TOVJjA8cP830ULZRd5Ng18t5hu5846Dd7UWzViKG7j9N5iue2MkoxTUChBdkatXyDbYJFWKKpdlBMUW6pWSZeNZ62fpNbWxbipZcRkEhnXpZqv9XBVgrvTjtE09iaa1vOQBTTU0vdo3QJ-k0goWgIutbd1a2evJeEXoLsUU-ldTr2eNp3Lc_etO3eo17WQjLsshcCYVboNk6jA2kcOX7h7sIrt2Rw",
+//         },
+//         // port: 443,
+//         url,
+//         body: postData,
+//         json: true
+//     };
 
-    const requestT = request(options, function (err, res, body) {
-        if (err) {
-            console.error('error posting json: ', err)
-            throw err
-        }
-        var headers = res.headers
-        var statusCode = res.statusCode
-        console.log('headers: ', headers)
-        console.log('statusCode: ', statusCode)
-        console.log('body: ', body)
-    })
+//     const requestT = request(options, function (err, res, body) {
+//         if (err) {
+//             console.error('error posting json: ', err)
+//             throw err
+//         }
+//         var headers = res.headers
+//         var statusCode = res.statusCode
+//         console.log('headers: ', headers)
+//         console.log('statusCode: ', statusCode)
+//         console.log('body: ', body)
+//     })
 
-    console.log('! + > + > + > + > + > + > !  : request', requestT)
+//     console.log('! + > + > + > + > + > + > !  : request', requestT)
 
-    // const requestT = request(options, function (err, res, body) {
-    //     if (err) {
-    //         console.error('error posting json: ', err)
-    //         throw err
-    //     }
-    //     var headers = res.headers
-    //     var statusCode = res.statusCode
-    //     console.log('headers: ', headers)
-    //     console.log('statusCode: ', statusCode)
-    //     console.log('body: ', body)
-    // })
+//     // const requestT = request(options, function (err, res, body) {
+//     //     if (err) {
+//     //         console.error('error posting json: ', err)
+//     //         throw err
+//     //     }
+//     //     var headers = res.headers
+//     //     var statusCode = res.statusCode
+//     //     console.log('headers: ', headers)
+//     //     console.log('statusCode: ', statusCode)
+//     //     console.log('body: ', body)
+//     // })
 
 
 
-});
+// });
 
-Given('a valid integration', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 1;
-});
+// Given('a valid integration', function () {
+//     // Write code here that turns the phrase above into concrete actions
+//     return 1;
+// });
 
-Given('a valid vehicle', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 1;
-});
+// Given('a valid vehicle', function () {
+//     // Write code here that turns the phrase above into concrete actions
+//     return 1;
+// });
 
-Given('a valid device', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 1;
-});
+// Given('a valid device', function () {
+//     // Write code here that turns the phrase above into concrete actions
+//     return 1;
+// });
 
-When('the device is associated to the vehicle', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 1;
-});
+// When('the device is associated to the vehicle', function () {
+//     // Write code here that turns the phrase above into concrete actions
+//     return 1;
+// });
 
-Then('the asset collection should be created.', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 1;
-});
+// Then('the asset collection should be created.', function () {
+//     // Write code here that turns the phrase above into concrete actions
+//     return 1;
+// });
